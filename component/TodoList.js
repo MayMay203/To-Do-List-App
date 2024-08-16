9ad2cdabd82d29c6bbed2df041173d5f20f42b6d
Nhung <hongnhung16052003@gmail.com>
2024-08-16 09:59:30 +0700

    toggle all action

--- component/TodoList.js
+++ component/TodoList.js
@@ -2,13 +2,20 @@ import html from '../core.js'
 import TodoItem from './TodoItem.js';
 import { connect } from '../store.js';
 
-function TodoList({todos}) {
+function TodoList({ todos, filters }) {
+    console.log(filters)
     return html`
         <section class="main">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
+            <input 
+                id="toggle-all" 
+                class="toggle-all" 
+                type="checkbox"
+                onchange="dispatch('toggleAll', this.checked)"
+                ${todos.every(filters.completed) && 'checked'}
+                >
             <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
-               ${todos.map(todo => TodoItem({todo}))}
+               ${todos.map((todo, index) => TodoItem({todo, index}))}
             </ul>
         </section>
     `
--- reducer.js
+++ reducer.js
@@ -1,13 +1,31 @@
 import storage from './util/storage.js'
 const init = {
-    todos: storage.get()
+    todos: storage.get(),
+    filter: 'all',
+    filters: {
+        all: () => true,
+        active: todo => !todo.completed,
+        completed: todo => todo.completed
+    }
 }
 
 const actions = {
     add({todos}, title) {
-        todos.push({ title, completed: false })
+        if (title) {
+            todos.push({ title, completed: false })
+            storage.set(todos)
+       }
+    },
+    toggle({ todos }, index) {
+        const todo = todos[index];
+        todo.completed = !todo.completed
+        storage.set(todos)
+    },
+    toggleAll({ todos }, completed) {
+        todos.forEach(todo => todo.completed = completed);
         storage.set(todos)
     }
+   
 }
 
 export default function reducer(state = init, action, args) {
